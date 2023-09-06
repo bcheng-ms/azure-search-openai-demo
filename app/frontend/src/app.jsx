57d5957a3148163a67822b0b7d98914e053d1d08
Brian Cheng <brian.cheng@microsoft.com>
2023-09-06 21:41:30 +0000

    try again

--- app/frontend/src/app.jsx
+++ app/frontend/src/app.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Route, useNavigate, Routes } from 'react-router-dom';
-import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
-import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import React from "react";
+import { Route, useNavigate, Routes } from "react-router-dom";
+import { Security, LoginCallback } from "@okta/okta-react";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 
 import Layout from "./pages/layout/Layout";
 import Chat from "./pages/chat/Chat";
@@ -9,55 +9,51 @@ import GetStarted from "./pages/getstarted/GetStarted";
 import Presentation from "./pages/presentation/Presentation";
 import OneShot from "./pages/oneshot/OneShot";
 import NoPage from "./pages/NoPage";
-import Home from './home';
-import Login from './pages/login';
-import Protected from './pages/protected';
-import config from './config';
-import Authenticated from './components/Authenticated';
+import Login from "./pages/login";
+import Protected from "./pages/protected";
+import config from "./config";
+import Authenticated from "./components/Authenticated";
+import Loading from "./components/Loading";
 
 const oktaAuth = new OktaAuth(config.oidc);
 
 const App = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const customAuthHandler = () => {
-    navigate('/login');
-  };
+    const customAuthHandler = () => {
+        navigate("/login");
+    };
 
-  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
-    navigate(toRelativeUrl(originalUri || '', window.location.origin), {replace:true});
-  };
+    const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+        navigate(toRelativeUrl(originalUri || "", window.location.origin), { replace: true });
+    };
 
-  return (
-    <div className="App">
-      <Security
-        oktaAuth={oktaAuth}
-        onAuthRequired={customAuthHandler}
-        restoreOriginalUri={restoreOriginalUri}
-      >
-        <Routes>
-            <Route path="/presentation/:name/:company" element={<Presentation />} />
-            {/* <Route path="/protected" element={<Layout />}>
+    return (
+        <div className="App">
+            <Security oktaAuth={oktaAuth} onAuthRequired={customAuthHandler} restoreOriginalUri={restoreOriginalUri}>
+                <Routes>
+                    <Route path="/presentation/:name/:company" element={<Presentation />} />
+                    {/* <Route path="/protected" element={<Layout />}>
                 <Route path="/" element={<GetStarted />} />
                 <Route path=":company/qa" element={<OneShot />} />
                 <Route path=":company" element={<Chat />} />
                 <Route path="*" element={<NoPage />} />
             </Route> */}
-            <Route path="/protected" element={ <Authenticated success={<Layout />} />}>
-                <Route index element={<Protected />} />
-            </Route>
-            <Route path="/login" render={() => <Login />} />
-            <Route path="/login/callback" element={<LoginCallback />} />
-            <Route path="/" element={<Layout />}>
-                <Route path="/" element={<GetStarted />} />
-                <Route path=":company/qa" element={<OneShot />} />
-                <Route path=":company" element={<Chat />} />
-                <Route path="*" element={<NoPage />} />
-            </Route>
-        </Routes>
-    </Security>
-    </div>
-  );
+                    <Route path="/protected" element={<Authenticated success={<Layout />} loading={<Loading />} />}>
+                        <Route index element={<Protected />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/login/callback" element={<LoginCallback />} />
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/" element={<GetStarted />} />
+                        <Route path=":company/qa" element={<OneShot />} />
+                        <Route path=":company" element={<Chat />} />
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+            </Security>
+        </div>
+    );
 };
 
 export default App;
--- app/frontend/src/pages/login/index.jsx
+++ app/frontend/src/pages/login/index.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import OktaSignInWidget from '../../components/OktaSignInWidget';
-import { useOktaAuth } from '@okta/okta-react';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import OktaSignInWidget from "../../components/OktaSignInWidget";
+import { useOktaAuth } from "@okta/okta-react";
 
-const Login = ({ config }) => {
-  const { oktaAuth, authState } = useOktaAuth();
-  const onSuccess = (tokens) => {
-    oktaAuth.handleLoginRedirect(tokens);
-  };
+const Login = () => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = tokens => {
+        oktaAuth.handleLoginRedirect(tokens);
+    };
 
-  const onError = (err) => {
-    console.log('Sign in error:', err);
-  };
+    const onError = err => {
+        console.log("Sign in error:", err);
+    };
 
-  if (!authState) {
-    return <div>Loading ... </div>;
-  }
+    if (!authState) {
+        return <div>Loading ... </div>;
+    }
 
-  return authState.isAuthenticated ?
-    <Navigate to={{ pathname: '/' }}/> :
-    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>;
+    return authState.isAuthenticated ? <Navigate to={{ pathname: "/" }} /> : <OktaSignInWidget onSuccess={onSuccess} onError={onError} />;
 };
 
 export default Login;
